f4b4131b58326bb5494e4b2e9f1f0c69020377fa
Mohamed Ali <64269343+Mohamed-Alyy@users.noreply.github.com>
2023-02-17 23:34:41 +0300

    Initial Commit

---
+++ littlelemon/littlelemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  littlelemonApp.swift
+//  littlelemon
+//
+//  Created by Mohamed Ali on 17/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct littlelemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
